ffd9da199ad551f31cde52ed748225ea66fad94b
Patryk <path1997@wp.pl>
2021-12-03 01:33:41 +0100

    final version

--- TwitterAppFront/src/app/main.service.ts
+++ TwitterAppFront/src/app/main.service.ts
@@ -10,7 +10,7 @@ import {Author} from "./author";
 })
 export class MainService {
 
-  private baseURL = "http://localhost:8080/api/v1";
+  private baseURL = "http://twitter-app-backend:8080/api/v1";
   constructor(private httpClient: HttpClient) { }
 
   getKeywordsList() :Observable<Keyword[]>{
